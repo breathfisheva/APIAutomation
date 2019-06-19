fb4a9d9609a0e99a6dce81976a618525521ac1b3
unknown <hongliu_goodluck@sina.com>
2019-06-19 13:46:06 +0800

    test case in yaml

---
+++ yamltestcase/loader.py
@@ -0,0 +1,27 @@
+import os
+import aiofiles, yaml, re, io
+
+def _check_format(file_path, content):
+    """ check testcase format if valid
+    """
+    # TODO: replace with JSON schema validation
+    if not content:
+        # testcase file content is empty
+        err_msg = u"Testcase file content is empty: {}".format(file_path)
+        # logger.log_error(err_msg)
+        # raise exceptions.FileFormatError(err_msg)
+
+    elif not isinstance(content, (list, dict)):
+        # testcase file content does not match testcase format
+        err_msg = u"Testcase file content format invalid: {}".format(file_path)
+        # logger.log_error(err_msg)
+        # raise exceptions.FileFormatError(err_msg)
+
+def load_yaml_file(yaml_file):
+    """ load yaml file and check file content format
+    """
+    with io.open(yaml_file, 'r', encoding='utf-8') as stream:
+        yaml_content = yaml.load(stream)
+        _check_format(yaml_file, yaml_content)
+        return yaml_content
+
